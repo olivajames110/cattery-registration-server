4fda273aa8f0ad12e320cb8d43b3392a82ae4098
olivajames110 <olivajames110@gmail.com>
2020-02-18 17:04:11 -0500

    added axios

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+const DUMMY_DATA = {
+	firstName : 'Jimmy',
+	lastName  : 'Oliva',
+	age       : 27
+};
+
+app.get('/api', (req, res) => {
+	console.log('Request Received');
+	console.log(req.body);
+	res.send(DUMMY_DATA);
+});
 
 app.listen(3000);
