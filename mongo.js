53fc721ef4fe4db48ecd33f6ecfa829a27f630b7
Jimmy Oliva <olivajames110@gmail.com>
2020-02-17 23:08:47 -0500

    added mongodb

---
+++ mongo.js
@@ -0,0 +1,10 @@
+const mongoClient = require("mongodb").MongoClient;
+//u: catpurrccinos
+//p: peanut225
+const url = "mongodb+srv://catpurrccinos:<peanut225>@cattery-registration-oiko3.mongodb.net/test?retryWrites=true&w=majority";
+const createProduct = async (req, res, next) => {};
+
+const getProducts = async (req, res, next) => {};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
