aa507402b85f62ed3f835910150a15702a0ab29c
James Oliva <jamesoliva@Jamess-MBP.home>
2020-03-09 23:57:22 -0400

    udate

--- models/product.js
+++ models/product.js
@@ -1,15 +1,32 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    dataUrl: {type: String, required: true},
-    dob_year: {type: String, required: true},
-    dob_month: {type: String, required: true},
-    dob_day: {type: String, required: true},
-    email: {type: String, required: true},
-})
+	// firstName: {type: String, required: true},
+	// lastName: {type: String, required: true},
+	// dataUrl: {type: String, required: true},
+	// dob_year: {type: String, required: true},
+	// dob_month: {type: String, required: true},
+	// dob_day: {type: String, required: true},
+	// email: {type: String, required: true},
+	// id: { type: Number, required: true },
+	visitType: { type: String, required: true },
+	minors: [
+		{
+			// id: { type: Number, required: true },
+			firstName: { type: String, required: false },
+			lastName: { type: String, required: false },
+			dob_year: { type: String, required: false },
+			dob_month: { type: String, required: false },
+			dob_day: { type: String, required: false }
+		}
+	],
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	dataUrl: { type: String, required: true },
+	dob_year: { type: String, required: true },
+	dob_month: { type: String, required: true },
+	dob_day: { type: String, required: true },
+	email: { type: String, required: true }
+});
 
-
-
-module.exports = mongoose.model('Product',productSchema);
+module.exports = mongoose.model('Product', productSchema);
--- mongoose.js
+++ mongoose.js
@@ -1,56 +1,71 @@
 const mongoose = require('mongoose');
 
-const Product = require('./models/product')
+const Product = require('./models/product');
 
-mongoose.connect('mongodb+srv://catpurrccinos:peanut225@cattery-registration-oiko3.mongodb.net/places_test?retryWrites=true&w=majority').then(()=>{
-    console.log("Connected to database");
-}).catch(()=>{
-    console.log('Something went wrong  ');
-});
+mongoose
+	.connect(
+		'mongodb+srv://catpurrccinos:peanut225@cattery-registration-oiko3.mongodb.net/places_test?retryWrites=true&w=majority'
+	)
+	.then(() => {
+		console.log('Connected to database');
+	})
+	.catch(() => {
+		console.log('Something went wrong  ');
+	});
 
-const createProduct = async (req,res,next) => {
-    console.log('create product run');
-    
-    const createdProduct = new Product({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        dataUrl: req.body.dataUrl,
-        dob_year: req.body.dob_year,
-        dob_month: req.body.dob_month,
-        dob_day: req.body.dob_day,
-        email: req.body.email,
+const createProduct = async (req, res, next) => {
+	console.log('create product run');
 
-    })
-    
-    console.log(createdProduct);
-    const result = await createdProduct.save();
-    // console.log('Result' , result);
+	const createdProduct = new Product({
+		visitType: req.body.visitType,
+		minors: [
+			{
+				// id: req.body.minors.id,
+				firstName: req.body.minors[0].firstName,
+				lastName: req.body.minors[0].lastName,
+				dob_year: req.body.minors[0].dob_year,
+				dob_month: req.body.minors[0].dob_month,
+				dob_day: req.body.minors[0].dob_day
+			}
+		],
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		dataUrl: req.body.dataUrl,
+		dob_year: req.body.dob_year,
+		dob_month: req.body.dob_month,
+		dob_day: req.body.dob_day,
+		email: req.body.email
+	});
 
-    res.json(result);
-}
+	console.log(createdProduct);
+	const result = await createdProduct.save();
+	// console.log('Result' , result);
 
-const getProducts = async (req,res,next) => {
-    console.log('get product run');
-    const products = await Product.find().exec();
-    // console.log(products[1].id);
-    res.json(products);
-}
-const deleteUser = async (req,res,next) => {
-    console.log('delete product run');
-    console.log(req.body.params);
-    let id = req.body.params;
-    const oldList = await Product.find().exec();
-    Product.deleteOne({_id: id}).exec();
-    const newList = await Product.find().exec();
-    // const newList = products.filter(user => {
-    //     return user.id !== id
-    // })
-    console.log(oldList.length);
-    // console.log(products.length);
-    console.log(newList.length);
-    
-    res.json(newList);
-}
+	res.json(result);
+};
+
+const getProducts = async (req, res, next) => {
+	console.log('get product run');
+	const products = await Product.find().exec();
+	// console.log(products[1].id);
+	res.json(products);
+};
+const deleteUser = async (req, res, next) => {
+	console.log('delete product run');
+	console.log(req.body.params);
+	let id = req.body.params;
+	const oldList = await Product.find().exec();
+	Product.deleteOne({ _id: id }).exec();
+	const newList = await Product.find().exec();
+	// const newList = products.filter(user => {
+	//     return user.id !== id
+	// })
+	console.log(oldList.length);
+	// console.log(products.length);
+	console.log(newList.length);
+
+	res.json(newList);
+};
 
 exports.deleteUser = deleteUser;
 exports.getProducts = getProducts;
